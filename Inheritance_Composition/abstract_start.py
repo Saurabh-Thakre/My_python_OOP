b1869017fac326ea980a743b226ee2b8f641cb44
Saurabh Thakre <thakre.sau@gmail.com>
2021-03-08 21:54:49 +0530

    ABC

---
+++ Inheritance_Composition/abstract_start.py
@@ -0,0 +1,32 @@
+# Using Abstract Base Classes to enforce class constraints
+from abc import ABC, abstractmethod
+
+class GraphicShape(ABC):
+    def __init__(self):
+        super().__init__()
+
+    @abstractmethod
+    def calcArea(self):
+        pass
+
+
+class Circle(GraphicShape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def calcArea(self):
+        return 3.14 * (self.radius ** 2)
+
+
+class Square(GraphicShape):
+    def __init__(self, side):
+        self.side = side
+
+    def calcArea(self):
+        return self.side * self.side
+
+
+c = Circle(10)
+print(c.calcArea())
+s = Square(12)
+print(s.calcArea())
